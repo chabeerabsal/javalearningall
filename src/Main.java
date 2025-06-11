8499a020ed9c234917099fb53c6cb81da9dc5e61
chabeerabsal <chabeerabsal@gmail.com>
2025-06-11 06:30:38 +0530

    javaLearning

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,37 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        System.out.printf("Hello and welcome!");
+//
+//        for (int i = 1; i <= 5; i++) {
+//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//            System.out.println("i = " + i);
+//            int a=10;
+//            char b='a';
+//            float f=.6f;
+//            System.out.println(f);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            int a=10;
-            char b='a';
-            float f=.6f;
-            System.out.println(f);
+            Car c= new Audi();
+            System.out.println(c);
+            System.out.println(c.a);
+            System.out.println(((Audi) c).cvar);
+            c.carbreak();
         }
     }
+
+abstract  class Car{
+    abstract public void carbreak();
+    int a=20;
+
+}
+class  Audi extends Car{
+    public void carbreak(){
+        System.out.println("Audi");
+    }
+    public void accelerator(){
+        System.out.println("Acceleratoe");
+    }
+    int cvar=200;
+  //  System.out.println(cvar);
 }
